9b93651f54b3e0b4e5b8d6f9f3fb9b5a796041d4
Alex Atsitty <atsitty.alex1128@gmail.com>
2022-03-15 23:34:10 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+
+  const lowerCase = word.toLowerCase()
+  console.log(lowerCase)
+
+  const backwards = lowerCase.split('').reverse().join('')
+  console.log(backwards)
+
+  return lowerCase === backwards
 }
 
 /* 
   Add your pseudocode here
+
+  -takes in a string
+  -makes sure its the same forward as it is backwards
+  -returns true if it does
+  -returns false if it isn't
 */
 
 /*
   Add written explanation of your solution here
+  -takes in a string and makes it all lower case
+  -turns the string into an array
+  -reverses the array
+  -puts the new array back together to make a new reveresed string
+  -compares the lowercase string to the new reversed string
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +40,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+console.log(isPalindrome('Hannah'))
